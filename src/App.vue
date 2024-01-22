5b6f23a0d7f3f8d1b17001ac6e46a4a463a2288d
cfxMusaib <musaib.ali@cloudfabrix.com>
2024-01-22 10:57:18 +0530

    Adding vue 3 playground

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import CytoCraph from './components/CytoGraph.vue'
+</script>
+
+<template>
+  <CytoCraph />
+</template>
---
+++ src/components/CytoGraph.vue
@@ -0,0 +1,70 @@
+<template>
+    <div style="height: 500px; width: 800px">
+        <div ref="cyto" class="cyto"></div>
+    </div>
+</template>
+  
+<script>
+import cytoscape from 'cytoscape';
+export default {
+    data: () => ({
+        cy: null
+    }),
+    mounted() {
+        var cy = cytoscape({
+            container: this.$refs.cyto, // container to render in
+
+            elements: [
+                // list of graph elements to start with
+                {
+                    // node a
+                    data: { id: 'a' },
+                },
+                {
+                    // node b
+                    data: { id: 'b' },
+                },
+                {
+                    // edge ab
+                    data: { id: 'ab', source: 'a', target: 'b' },
+                },
+            ],
+
+            style: [
+                // the stylesheet for the graph
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': '#666',
+                        label: 'data(id)',
+                    },
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        width: 3,
+                        'line-color': '#ccc',
+                        'target-arrow-color': '#ccc',
+                        'target-arrow-shape': 'triangle',
+                        'curve-style': 'bezier',
+                    },
+                },
+            ],
+
+            layout: {
+                name: 'grid',
+                rows: 1,
+            },
+        });
+        this.cy = cy
+    },
+};
+</script>
+  
+<style>
+.cyto {
+    height: 100%;
+    width: 100%;
+}
+</style>
+  
